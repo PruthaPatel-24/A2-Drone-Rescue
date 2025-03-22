ec3e595daebd4e963a7f5da2c48f8724d11c4f18
Ronit <pater161@mcmaster.ca>
2025-03-22 17:42:21 -0400

    Added final report for the closest creek to emergency site. Fix #19

--- src/main/java/ca/mcmaster/se2aa4/island/teamXXX/FinalReport.java
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/FinalReport.java
@@ -1,13 +1,14 @@
 package ca.mcmaster.se2aa4.island.teamXXX;
 
 public class FinalReport {
+
     IslandMap map;
+
     public FinalReport(IslandMap m) {
         this.map = m;
     }
-    public String finalReport() {
-        int[] closest_creek = map.closestCreek();
 
-        return "closest creek found to emergency site is at x: " + closest_creek[0] + " y: " + closest_creek[1];
-    }   
+    public String finalReport() {;
+        return map.closestCreek();
+    }
 }
